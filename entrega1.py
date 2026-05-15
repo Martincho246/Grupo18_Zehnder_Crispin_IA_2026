ffa85e92da2b2af625ba962acd11eca7c6d7c813
Martincho246 <mzehnder2003@gmail.com>
2026-05-15 20:55:20 -0300

    optimizacion heuristica y solucion de errores

--- entrega1.py
+++ entrega1.py
@@ -1,4 +1,5 @@
 from simpleai.search.models import SearchProblem
+from simpleai.search import SearchProblem, astar
 
 Max_Bateria = 20
 
@@ -6,12 +7,10 @@ Zona_Sombra = []
 
 CapMax_Carga = 2
 
-Costo_Bateria = {"moverse": 1, "sobremarcha": 4, "equipar": 1, "recolectar": 3, "depositar": 1, "recargar": 10}
+Costo_Bateria = {"moverse": 1, "sobremarcha": 4, "equipar": 1, "recolectar": 3, "depositar": 1, "recargar": -10}
 
 Costo_Minutos = {"moverse": 1, "sobremarcha": 1, "equipar": 3, "recolectar": 2, "depositar": 1, "recargar": 4}
 
-Max_Y = 3
-Max_X = 3
 
 class RoverProblem(SearchProblem):
     #pensar estado.
@@ -41,19 +40,17 @@ class RoverProblem(SearchProblem):
         )
         posibles_equipasiones = (
             "termico",
-            "percusión"
+            "percusion"
         )
 
-        for move in posibles_movimientos:
-            nueva_posicion = (posicion_rover[0] + move[0], posicion_rover[1] + move[1])
-
-            if nueva_posicion[0] <= Max_X and nueva_posicion[1] <= Max_Y and bateria_actual - Costo_Bateria["moverse"] > 0 and nueva_posicion[0] >= 0 and nueva_posicion[1] >=0:
+        if bateria_actual - Costo_Bateria["moverse"] > 0:
+            for move in posibles_movimientos:
+                nueva_posicion = (posicion_rover[0] + move[0], posicion_rover[1] + move[1])
                 acciones_validas.append(("moverse", nueva_posicion))
         
-        for move in posibles_sobremarchas:
-            nueva_posicion = (posicion_rover[0] + move[0], posicion_rover[1] + move[1])
-
-            if nueva_posicion[0] <= Max_X and nueva_posicion[1] <= Max_Y and bateria_actual - Costo_Bateria["sobremarcha"] > 0 and nueva_posicion[0] >= 0 and nueva_posicion[1] >=0:
+        if bateria_actual - Costo_Bateria["sobremarcha"] > 0:
+            for move in posibles_sobremarchas:
+                nueva_posicion = (posicion_rover[0] + move[0], posicion_rover[1] + move[1])
                 acciones_validas.append(("sobremarcha", nueva_posicion))
         
         for equip in posibles_equipasiones:
@@ -63,18 +60,20 @@ class RoverProblem(SearchProblem):
         if taladro_activo == "termico":
             if posicion_rover in muestras_igneas and carga_actual < CapMax_Carga and bateria_actual - Costo_Bateria["recolectar"] > 0:
                 acciones_validas.append(("recolectar", "ignea"))
-        elif taladro_activo == "percusión":
+        elif taladro_activo == "percusion":
             if posicion_rover in muestras_sedimentarias and carga_actual < CapMax_Carga and bateria_actual - Costo_Bateria["recolectar"] > 0:
                 acciones_validas.append(("recolectar", "sedimentaria"))
         
-        if carga_actual == CapMax_Carga and bateria_actual - Costo_Bateria["recolectar"] > 0:
-            acciones_validas.append(("entregar", None))
+        if carga_actual == CapMax_Carga and bateria_actual - Costo_Bateria["depositar"] > 0:
+            acciones_validas.append(("depositar", None))
 
-        if carga_actual == 1 and len(muestras_igneas) == 0 and len(muestras_sedimentarias) == 0:
-            acciones_validas.append(("entregar", None))
+        if carga_actual == 1 and len(muestras_igneas) == 0 and len(muestras_sedimentarias) == 0 and bateria_actual - Costo_Bateria["depositar"] > 0:
+            acciones_validas.append(("depositar", None))
 
         if posicion_rover not in Zona_Sombra:
             acciones_validas.append(("recargar", None))
+
+        return acciones_validas
 
     def cost(self, state1, action, state2):
         #costos_minutos
@@ -97,62 +96,89 @@ class RoverProblem(SearchProblem):
         bateria_actual = state [1]
         taladro_activo = state [2]
         carga_actual = state[3]
-        muestras_igneas = state[4]
-        muestras_sedimentarias = state[5]
+        muestras_igneas = list(state[4])
+        muestras_sedimentarias = list(state[5])
+
+        bateria_actual = bateria_actual - Costo_Bateria[action[0]]
 
         if action[0] == "moverse" or action[0] == "sobremarcha":
             posicion_rover = action[1]
-            bateria_actual = bateria_actual - Costo_Bateria[action[0]]
         elif action[0] == "equipar":
             taladro_activo = action[1]
-            bateria_actual = bateria_actual - Costo_Bateria[action[0]]
         elif action[0] == "recolectar":
             if action[1] == "ignea":
-                muestras_igneas = muestras_igneas.remove(posicion_rover)
+                muestras_igneas.remove(posicion_rover)
             else:
-                muestras_sedimentarias = muestras_sedimentarias.remove(posicion_rover)
+                muestras_sedimentarias.remove(posicion_rover)
+            carga_actual +=1
         elif action[0] == "depositar":
             carga_actual = 0
-            bateria_actual = bateria_actual - Costo_Bateria[action[0]]
         elif action[0] == "recargar":
-            bateria_actual = bateria_actual + Costo_Bateria[action[0]]
+            if bateria_actual > Max_Bateria:
+                bateria_actual = Max_Bateria
 
-        return tuple(posicion_rover, bateria_actual, taladro_activo, carga_actual, muestras_igneas, muestras_sedimentarias)
+        return (posicion_rover, bateria_actual, taladro_activo, carga_actual, tuple(muestras_igneas), tuple(muestras_sedimentarias))
 
     def heuristic(self, state):
         #(posicionrover(X,Y), bateria int,taladroactivo string ,cargaactual int ,muestrasigneas [(X1, Y1), (X2, Y2), (Xn, Yn)], muestrassedimentarias [(X1, Y1), (X2, Y2), (Xn, Yn)])
         posicion_rover = state [0]
-        #bateria_actual = state [1]
+        bateria_actual = state [1]
         taladro_activo = state [2]
         carga_actual = state[3]
         muestras_igneas = state[4]
         muestras_sedimentarias = state[5]
         #por cada muestra en el piso el costo de buscarla seria la distancia manhattan entre 2(la sobremarcha) + costo de cambiar el taladro si amerita.
         costo_unitario_max = 0
+        costo_bateria_local = 0
+
         for muestra in muestras_igneas:
             costo_muestra = 0
+            costo_bateria_muestra = 0
             if taladro_activo != "termico":
                 costo_muestra += Costo_Minutos["equipar"]
+                costo_bateria_muestra += Costo_Bateria["equipar"]
             
             distance = abs(muestra[0] - posicion_rover[0]) + abs(muestra[1] - posicion_rover[1])
-            costo_muestra += (distance/2) * Costo_Minutos["sobremarcha"] 
+            costo_muestra += (distance/2) * Costo_Minutos["sobremarcha"]
+            costo_bateria_muestra += (distance) * Costo_Bateria["moverse"]
             
             if costo_muestra > costo_unitario_max:
+                costo_bateria_local = costo_bateria_muestra
                 costo_unitario_max = costo_muestra
             
         for muestra in muestras_sedimentarias:
             costo_muestra = 0
-            if taladro_activo != "percusión":
+            costo_bateria_muestra = 0
+            if taladro_activo != "percusion":
                 costo_muestra += Costo_Minutos["equipar"]
+                costo_bateria_muestra += Costo_Bateria["equipar"]
             
             distance = abs(muestra[0] - posicion_rover[0]) + abs(muestra[1] - posicion_rover[1])
-            costo_muestra += (distance/2) * Costo_Minutos["sobremarcha"] 
+            costo_muestra += (distance/2) * Costo_Minutos["sobremarcha"]
+            costo_bateria_muestra += (distance) * Costo_Bateria["moverse"]
             
             if costo_muestra > costo_unitario_max:
+                costo_bateria_local = costo_bateria_muestra
                 costo_unitario_max = costo_muestra
         
         #para todos (carga actual + cantida de muestras en el piso)/2 * costo de deposito. recolectar al menos una vez por cada cosa en el piso.
         cantidad_muestras = len(muestras_igneas) + len(muestras_sedimentarias)
-        costo_global = ((carga_actual + cantidad_muestras)/2 ) * Costo_Minutos["depositar"] + cantidad_muestras * Costo_Minutos["recolectar"]
+        costo_bateria = cantidad_muestras * Costo_Bateria["recolectar"] + ((carga_actual + cantidad_muestras)/2 ) * Costo_Bateria["depositar"]
+        costo_global = (carga_actual + cantidad_muestras ) * Costo_Minutos["depositar"] + cantidad_muestras * Costo_Minutos["recolectar"]
+        bateria_actual = bateria_actual - costo_bateria - costo_bateria_local
 
+        while bateria_actual < 0:
+            bateria_actual = bateria_actual - Costo_Bateria["recargar"]
+            costo_global += Costo_Minutos["recargar"]
+        
         return costo_global + costo_unitario_max
+
+def planear_rover(rover_inicio, bateria_inicial, zonas_sombra, muestras_igneas, muestras_sedimentarias):
+    EstadoInicial = (rover_inicio, bateria_inicial, "ninguno", 0, tuple(muestras_igneas), tuple(muestras_sedimentarias))
+    
+    global Zona_Sombra
+    Zona_Sombra = zonas_sombra
+
+    Problema = RoverProblem(EstadoInicial)
+    Resultado = astar(Problema)
+    return [accion for accion, estado in Resultado.path()[1:]]
